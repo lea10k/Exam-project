7d35fd90b1c409313d41c3540a8e37c7f565e690
Lea <leakoedel19@gmail.com>
2025-04-09 20:26:37 +0200

    Modify script for disappearing platform

--- Assets/Scripts/DisappearingPlattform.cs
+++ Assets/Scripts/DisappearingPlattform.cs
@@ -2,32 +2,26 @@ using UnityEngine;
 
 public class DisappearingPlattform : MonoBehaviour
 {
-    public float timeToTogglePlatform = 2;
-    public float currentTime = 0;
-    public new bool enabled = true;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        enabled = true;
-    }
+    public float timeToTogglePlatform = 2f;
+    private float currentTime = 0f;
+    private bool platformVisible = true;
 
-    // Update is called once per frame
     void Update()
     {
         currentTime += Time.deltaTime;
 
-        if(currentTime >= Time.deltaTime){
-            currentTime = 0;
+        if (currentTime >= timeToTogglePlatform)
+        {
+            currentTime = 0f;
             TogglePlatform();
         }
     }
 
-    void TogglePlatform(){
-        enabled = !enabled;
-        foreach(Transform child in gameObject.transform){
-            if(child.tag != "Player"){
-                child.gameObject.SetActive(enabled);
-            }
-        }
+    void TogglePlatform()
+    {
+        platformVisible = !platformVisible;
+
+        GetComponent<SpriteRenderer>().enabled = platformVisible;
+        GetComponent<BoxCollider2D>().enabled = platformVisible;
     }
 }
