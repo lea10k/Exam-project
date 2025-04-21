9f7cb13ace1bf99ae98efa610bfbfb84125e9c19
Lea <leakoedel19@gmail.com>
2025-04-21 12:18:40 +0200

    Insert new PU assets, create animation and change script

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -23,11 +23,7 @@ public class PowerUp : MonoBehaviour
     }
 
     IEnumerator Pickup(Collider2D player){
-         // 1) Spawn effect
-        GameObject effect = Instantiate(pickupEffect, transform.position, transform.rotation);
-        // Destroy effect after animation => unless, its last frame stays on the screen 
-        Destroy(effect, 0.25f);
-
+    
         // 2) Fetch the player's Movement component
         var movement = player.GetComponent<PlayerMovement>();
         if (movement != null)
@@ -35,15 +31,24 @@ public class PowerUp : MonoBehaviour
             // 3) Apply the right power-up
             switch (powerUpType)
             {
-                /*case PowerUpType.Trampoline:
+                case PowerUpType.Trampoline:
+                     // 1) Spawn effect
+                    GameObject effect1 = Instantiate(pickupEffect, transform.position, Quaternion.Euler(0, 0, 90));
+                    // Destroy effect after animation => unless, its last frame stays on the screen 
+                    Destroy(effect1, 0.317f);
                     //movement.EnableTrampoline();   
                     break;
 
-                case PowerUpType.Wings:
+                /*case PowerUpType.Wings:
                     //movement.EnableWings();        
                     break; */
 
                 case PowerUpType.DoubleJump:
+                     // 1) Spawn effect
+                    GameObject effect3 = Instantiate(pickupEffect, transform.position, transform.rotation);
+                    // Destroy effect after animation => unless, its last frame stays on the screen 
+                    Destroy(effect3, 0.25f);
+
                     movement.EnableDoubleJump();
                     doubleJumpActive = true;
 
